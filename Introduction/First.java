29b3ca3cd665efd8769877d23bc181b73b69f7a2
crs562 <acs.chaitanya35@gmail.com>
2019-01-14 16:28:57 -0500

    first lecture

---
+++ Introduction/First.java
@@ -0,0 +1,11 @@
+/*
+Program to display a welcome message
+Author: acs.chaitanya35@gmail.com
+Date: 31st July 2017
+*/
+
+class First {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Java");
+	}
+}
